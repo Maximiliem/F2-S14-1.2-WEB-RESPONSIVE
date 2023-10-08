95d73e5bfae18aca9b40551363d44ce5ddd7a0e0
Maximiliem <maximiliem@yahoo.com>
2023-10-07 23:26:58 -0300

    Add: navbar and styles on text

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-  document.getElementById("burger").addEventListener("click", function hiddenElements() {
+document.getElementById("burger").addEventListener("click", function hiddenElements() {
     var x = document.getElementById("items");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  });
+});
 
-  document.getElementById("opciones").addEventListener("click", function hiddenElements() {
+document.getElementById("opciones").addEventListener("click", function hiddenElements() {
     var x = document.getElementById("opciones-desplegadas");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  });
+});
