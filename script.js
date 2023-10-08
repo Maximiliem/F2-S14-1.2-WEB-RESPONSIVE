0b29b61bad540a4957ba0691dab7c227948c3ea8
Maximiliem <maximiliem@yahoo.com>
2023-10-08 18:25:24 -0300

    me falta hacer que el botón de opciones tenga un collapse

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-document.getElementById("burger").addEventListener("click", function hiddenElements() {
-    var x = document.getElementById("items");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+// INICIO DE DECLARACIÓN DE CONSTANTES
+const containerOptions = document.getElementById('visibleOptionsContainer');
+const optionsContent = 
+    `
+    <div class="col-lg-3" id="optionsItems">
+        <div class="col-lg-3 opciones">
+            <ul>
+                <div class="row option-list">
+                    <a href="#" class="option-item">Opción 1</a>
+                </div>
+                <div class="row option-list">
+                    <a href="#" class="option-item">Opción 2</a>
+                </div>
+                <div class="row option-list">
+                    <a href="#" class="option-item">Opción 3</a>
+                </div>
+                <div class="row option-list">
+                    <a href="#" class="option-item">Opción 4</a>
+                </div>
+            </ul>
+        </div>
+    </div>`;
+
+const showOptionsElements = document.createElement('div');
+const showOptionsOnBtn = document.getElementById('btnDesplegarOpciones');
+// FIN DE DECLARACIÓN DE CONSTANTES
+
+// INICIO DE FUNCIONES
+function putOptionContent(){
+    containerOptions.innerHTML = optionsContent;
+}
+putOptionContent();
+
+function putOptionContentOnbtn(){
+    showOptionsOnBtn.appendChild(showOptionsElements);
+    showOptionsElements.classList.add("card", "card-body"); // Agregar clases al elemento showOptionsElements
+    showOptionsElements.setAttribute("id", "btnDivId");
+}
+putOptionContentOnbtn();
+
+document.getElementById('optionsLink').addEventListener('click', function() {
+    const optionsCollapse = document.getElementById('optionsItems');
+    const isCollapsed = optionsCollapse.classList.contains('show');
+    if (isCollapsed) {
+        optionsCollapse.classList.remove('show');
     } else {
-        x.style.display = "block";
+        optionsCollapse.classList.add('show');
     }
 });
 
-document.getElementById("opciones").addEventListener("click", function hiddenElements() {
-    var x = document.getElementById("opciones-desplegadas");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-});
+// FIN DE FUNCIONES
